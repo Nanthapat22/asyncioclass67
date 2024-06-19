4790c9f33f600b23e8b41d50ec5f1ec3c38cc165
Nanthapat <153785290+Nanthapat22@users.noreply.github.com>
2024-06-19 12:04:10 +0700

    ส่งการบ้านครั้ง 2

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,13 @@
 # running a function with arguments in another thread
+from time import sleep,ctime 
+from threading import Thread
+
+def task (sleep_time, message):
+    sleep(sleep_time)
+    print(f'{ctime()} {message}')
+    
+thread = Thread(target=task, args=(1.5,'New message from another thre'))
+
+thread.start()
+print(f'{ctime()} waiting for the thread...')
+thread.join()
